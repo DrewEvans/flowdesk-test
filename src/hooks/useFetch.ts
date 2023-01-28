70770be4597e38cb44fa4a8b6b7bead4c44aa3c9
Andrew E <andrew.wr.evans@gmail.com>
2023-01-28 12:01:29 +0100

    updating useFetch for ts w/ config

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,55 +1,37 @@
+
+import  axios from 'axios'
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 
-async function useFetch<TResponse>(url: string) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+export const  useFetch = (url: string, symbol: string) => {
+    const [data, setData] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  //call function to update the data on first render from the API
-  useEffect(() => {
-    //setloading to true
-    setLoading(true);
-    //make call to the URL to get the data
-    axios
-      .get(url)
-      //if response received return set state with the response
-      .then((response) => {
-        setData(response.data);
-      })
-      //if error return error message
-      .catch((err) => {
-        setError(err);
-      })
-      //once one of the two req are meet, set loading to false
-      .finally(() => {
-        setLoading(false);
-      });
-    //re-render if URL is change after load
-  }, [url]);
+    const config = {
+        method: 'get',
+        url: `https://data.binance.com/${url}?symbol=${symbol}`,
+        // headers: { }
+      };
 
-  //call function to update the data from the API
-  const refetch = () => {
-    //data is being loaded
-    setLoading(true);
-    //make call to the URL to get the data
-    axios
-      .get(url)
-      //if response received return set state with the response
-      .then((response) => {
-        setData(response.data);
-      })
-      //if error return error message
-      .catch((err) => {
-        setError(err);
-      })
-      //once one of the two req are meet, set loading to false
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+      useEffect(() => {
+        //setloading to true
+        setLoading(true);
+        //make call to the URL to get the data
+        axios(config)
+          //if response received return set state with the response
+          .then((response :any) => {
+            setData(response.data);
+          })
+          //if error return error message
+          .catch((err: any) => {
+            setError(err);
+          })
+          //once one of the two request are met, set loading to false
+          .finally(() => {
+            setLoading(false);
+          });
+        //re-render if URL is change after load
+      }, [url]);
 
-  return { data, loading, error, refetch };
+    return data;
 }
-
-export default useFetch;
