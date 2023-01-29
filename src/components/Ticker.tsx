1464153b04b2822c655019d9d4be243afe1f18cd
Andrew E <andrew.wr.evans@gmail.com>
2023-01-29 20:45:53 +0100

    attempt to make ticker data dynamic

--- src/components/Ticker.tsx
+++ src/components/Ticker.tsx
@@ -1,4 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const tickerAnimation = keyframes`
+    0% {
+      -webkit-transform: translate3d(0, 0, 0);
+      transform: translate3d(0, 0, 0);
+      visibility: visible;
+    }
+  
+    100% {
+      -webkit-transform: translate3d(-100%, 0, 0);
+      transform: translate3d(-100%, 0, 0);
+    }
+  }
+`
+
+const TickerContainer = styled.div`
+    display: inline-block;
+    height: 4rem;
+    line-height: 4rem;  
+    white-space: nowrap;
+    padding-right: 100%;
+    box-sizing: content-box;
+
+    animation-name: ${tickerAnimation};
+    animation-duration: 70s;
+    animation-iteration-count: infinite;
+`
+
+const TickerItem = styled.div`   
+display: inline-block;
+padding: 0 2rem;
+font-size: 1.5rem;
+color: white;  
+`
 
 type TickerProps = {
     data: any
@@ -6,6 +41,8 @@ type TickerProps = {
 }
 
 const Ticker = ({ data }: TickerProps) => {
+    useEffect(() => { console.log("Refreshed") }, [data])
+
     const entries = getObjectEntries(data);
 
     function getObjectEntries<T>(obj: Object): Array<[keyof T, T[keyof T]]> {
@@ -13,15 +50,15 @@ const Ticker = ({ data }: TickerProps) => {
     }
 
     return (
-        <div>
-            <ul>
-                {entries.map(([key, value]) => (
-                    <li key={key}>
-                        {key}: {value}
-                    </li>
-                ))}
-            </ul>
-        </div>
+
+        <TickerContainer>
+            {entries.map(([key, value]) => (
+                <TickerItem key={key}>
+                    {key}: {typeof value === "string" ? parseFloat(value).toFixed(2) : value}
+                </TickerItem>
+            ))}
+        </TickerContainer>
+
     )
 }
 
