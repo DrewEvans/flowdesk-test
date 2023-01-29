f26c7defc2e3d2be37c3a06d68f2f08dd9f4077d
Andrew E <andrew.wr.evans@gmail.com>
2023-01-29 11:21:12 +0100

    test ticker component

--- src/components/Ticker.tsx
+++ src/components/Ticker.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Ticker = () => {
+type TickerProps = {
+    data: any
+    // onChange: (Value: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+const Ticker = ({ data }: TickerProps) => {
+    const { closeTime, count, lastPrice, lowPrice, openTime, priceChange } = data
+
+
     return (
-        <div>Ticker</div>
+        <div>
+            <ul>
+                <li>{closeTime}</li>
+                <li>{count}</li>
+                <li>{lastPrice}</li>
+                <li>{lowPrice}</li>
+                <li>{openTime}</li>
+                <li>{priceChange}</li>
+            </ul>
+        </div>
     )
 }
 
