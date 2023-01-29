7f87b902b6bcdf728c91c4e7a5d4f0a5f2eff691
Andrew E <andrew.wr.evans@gmail.com>
2023-01-29 15:30:55 +0100

    updated styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { sortArray } from '../../helpers/sortArray'
+import styled from 'styled-components';
 import "./table.css"
+
+const Select = styled.select`
+background-color: white;
+border: none;
+padding: .5em .25em ;
+margin: .5em 0;
+width: 25%;
+font-family: inherit;
+font-size: inherit;
+font-weight: bold;
+color: #6b00d2;
+cursor: inherit;
+line-height: inherit;
+`;
+
+const TableTitleDisplay = styled.div`
+display: flex;
+flex-direction: column;
+`
 
 type TableProps = {
     data: any
@@ -28,14 +48,16 @@ const Table = ({ data, }: TableProps) => {
 
     return (
         <>
-            <div>
-                <select onChange={handleChange}>
+            <TableTitleDisplay>
+                <h2>Recent Trades</h2>
+                <label>Sort By </label>
+                <Select onChange={handleChange}>
                     <option value="time">Time</option>
                     <option value="price">Price</option>
                     <option value="qty">Quantity</option>
                     <option value="quoteQty">Quote Quantity</option>
-                </select>
-            </div>
+                </Select>
+            </TableTitleDisplay>
 
             <table>
                 <thead>
@@ -52,10 +74,8 @@ const Table = ({ data, }: TableProps) => {
                         sortedData.map((rows: any) => {
                             const { time, price, quoteQty, qty } = rows
 
-                            console.log()
-
                             return (
-                                <tr>
+                                <tr className='row-data'>
                                     <td>{moment(time).format("DD/MM/YYYY")}</td>
                                     <td>{moment(time).format("LTS")}</td>
                                     <td>{parseFloat(price)}</td>
